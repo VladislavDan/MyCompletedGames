29438a27d0e14b51c7b84c3a4cf1f0d6b1840ed8
VladislavDanilov <Vladislav_Danilov@epam.com>
2017-10-16 01:50:33 +0400

    Created function for receiving auth code for auth2 authorization of the google.

--- MyCompletedGames/app/app.component.ts
+++ MyCompletedGames/app/app.component.ts
@@ -1,8 +1,31 @@
+require('nativescript-nodeify');
 import {Component} from "@angular/core";
+import Application = require("application");
+import SocialLogin = require("nativescript-social-login");
 
 @Component({
     selector: "ns-app",
     templateUrl: "app.component.html"
 })
 export class AppComponent {
+
+    ngOnInit() {
+        SocialLogin.addLogger(function (msg: any, tag: string) {
+            console.log('[nativescript-social-login]: (' + tag + '): ' + msg);
+        });
+        if (Application.android) {
+            let result = SocialLogin.init({
+                google: {
+                    serverClientId: "144292527666-9h1ugthmsl2qjdlpmv5cl3o8gps44f55.apps.googleusercontent.com",
+                    isRequestAuthCode: true
+                }
+            });
+            console.dir(result);
+            SocialLogin.loginWithGoogle(
+                (result) => {
+                    console.dir(result);
+                }
+            );
+        }
+    }
 }
