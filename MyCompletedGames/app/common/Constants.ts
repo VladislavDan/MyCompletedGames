3633707dc68595970f967a17bf3cf230b428fcb3
Vladislav Danilov <Vladislav_Danilov@epam.com>
2017-10-24 13:22:30 +0400

    Created UI for filters. Created list of consoles.

--- MyCompletedGames/app/common/Constants.ts
+++ MyCompletedGames/app/common/Constants.ts
@@ -3,3 +3,24 @@ export const GAME_FILE_NAME = "my-completed-games.json";
 export const DATE_CHANGED_FORMAT = "YYYY-MM-DDTHH:mm:ss";
 
 export const TOKEN_KEY = "TOKEN_KEY";
+
+export const VIDEO_GAME_CONSOLES = [
+
+    "WiiU",
+    "Game Cube & Wii",
+    "Nintendo 3DS",
+    "Nintendo DS",
+    "Super Nintendo",
+    "Nintendo Famicom",
+    "Game Boy Advanced",
+    "Game Boy Color & Game Boy",
+    "Personal Computer",
+    "Sony Playstation 3",
+    "Sony Playstation 2",
+    "Sony Playstation One",
+    "Sega Dreamcast",
+    "Sega Saturn",
+    "Sega Mega Drive & Sega 32x",
+    "Sega Master System",
+    "Arcade Machine"
+];
